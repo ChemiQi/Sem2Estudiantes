c2785031d6167ca0854dcdce3c3b9c039f8c8d60
chema.martinez <chema.martinez@bosonit.com>
2021-05-04 11:44:05 +0200

    Proyecto creado

---
+++ src/main/java/com/example/Sem2Estudiantes/Sem2EstudiantesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sem2Estudiantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem2EstudiantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem2EstudiantesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Sem2Estudiantes/Sem2EstudiantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sem2Estudiantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem2EstudiantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
