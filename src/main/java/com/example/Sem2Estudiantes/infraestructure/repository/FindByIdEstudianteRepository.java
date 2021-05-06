d929e076c376e672f6148744b7297b8d3e79da63
chema.martinez <chema.martinez@bosonit.com>
2021-05-06 10:24:31 +0200

    FindById JDBC, mirar error con fecha y double

--- src/main/java/com/example/Sem2Estudiantes/infraestructure/repository/FindByIdEstudianteRepository.java
+++ src/main/java/com/example/Sem2Estudiantes/infraestructure/repository/FindByIdEstudianteRepository.java
@@ -6,15 +6,19 @@ import com.example.Sem2Estudiantes.infraestructure.repository.jpa.EstudianteRepo
 import com.example.Sem2Estudiantes.infraestructure.repository.port.FindByIdEstudiantePort;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Optional;
 
 @Repository
 public class FindByIdEstudianteRepository implements FindByIdEstudiantePort {
 
     @Autowired
-    EstudianteRepository estudianteRepository;
+    private JdbcOperations jdbc;
 
     @Override
     public EstudianteDtoOutput findByIdEstudiante(Integer id) {
@@ -22,13 +26,37 @@ public class FindByIdEstudianteRepository implements FindByIdEstudiantePort {
     }
 
     private EstudianteDtoOutput obtenerEstudianteDtoOutputByIdEstudiante(Integer id){
-        Optional<EstudianteJpa> estudianteJpaOptional = estudianteRepository.findById(id);
+        EstudianteJpa estudianteEncontrado = jdbc.queryForObject("select * from Estudiante_Jpa where id= ?",new estudianteRowMapper(),id);
+        return new EstudianteDtoOutput(estudianteEncontrado);
+
+        /*Optional<EstudianteJpa> estudianteJpaOptional = estudianteRepository.findById(id);
         if(estudianteJpaOptional.isPresent()){
 
             return new EstudianteDtoOutput(estudianteJpaOptional.get());
         }else{
             return null;
+        }*/
+    }
+
+    private class estudianteRowMapper implements RowMapper<EstudianteJpa>{
+
+        @Override
+        public EstudianteJpa mapRow(ResultSet resultSet, int i) throws SQLException {
+            return new EstudianteJpa(
+                    resultSet.getInt("id"),
+                    resultSet.getString("nombre"),
+                    resultSet.getString("apellido"),
+                    resultSet.getString("correo"),
+                    //new java.util.Date(resultSet.getTime("fechaEntrada")),
+                    resultSet.getDate("fechaEntrada"),
+                    resultSet.getString("ciudad"),
+                    resultSet.getDouble("horasSemanales"),
+                    resultSet.getString("especialidad"),
+                    resultSet.getString("estado")
+            );
         }
     }
-    
+
 }
+
+
